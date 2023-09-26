d16c2b077dd7f4a9c26d195c6eed14e7516c4583
JunLee8108 <lejhn1@gmail.com>
2023-09-25 23:08:41 -0500

    updated responsiveness and still working on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { feature, testimonial, course } from "../../util/data/data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightLong, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -18,7 +19,6 @@ export default function Home() {
               <h2 className={styles.headingTwo}>
                 E-Learning Platform with Adaptive Learning!
               </h2>
-              {/* <FontAwesomeIcon icon={faRightLong} className={styles.icon} /> */}
             </div>
           </div>
         </div>
@@ -29,30 +29,16 @@ export default function Home() {
         <div className={styles.featuresHeader}>
           <h3>Core Features of VocaVista</h3>
         </div>
+
         <div className={styles.featuresContainer}>
-          <div className={styles.featureBox}>
-            <h3>🟣 Adaptive Learning</h3>
-            <p>
-              Experience tailored lessons that adapt to your individual learning
-              pace.
-            </p>
-          </div>
-
-          <div className={styles.featureBox}>
-            <h3>🟠 Interactive Lessons</h3>
-            <p>
-              Engage in lessons filled with dynamic quizzes, speaking exercises,
-              and more.
-            </p>
-          </div>
-
-          <div className={styles.featureBox}>
-            <h3>🟢 Global Community</h3>
-            <p>
-              Join a community of language learners and native speakers to
-              practice with.
-            </p>
-          </div>
+          {feature.map((content, index) => {
+            return (
+              <div className={styles.featureBox} key={index}>
+                <h3>{feature[index].title}</h3>
+                <p>{feature[index].content}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
 
@@ -62,75 +48,46 @@ export default function Home() {
           <h3 className={styles.testimonialsSectionTitle}>
             What Our Users Say
           </h3>
-          <div className={styles.testimonial}>
-            <FontAwesomeIcon icon={faPlayCircle} className={styles.playIcon} />
-            <p>
-              "I've never learned a language so efficiently! The adaptive
-              learning really makes a difference."
-            </p>
-            <span>- Sarah, New York</span>
-          </div>
-          <div className={styles.testimonial}>
-            <FontAwesomeIcon icon={faPlayCircle} className={styles.playIcon} />
-            <p>
-              "The community here is so welcoming. I've made friends and
-              language partners from all over the world."
-            </p>
-            <span>- Jae, Seoul</span>
-          </div>
+
+          {testimonial.map((content, index) => {
+            return (
+              <div className={styles.testimonial} key={index}>
+                <FontAwesomeIcon
+                  icon={faPlayCircle}
+                  className={styles.playIcon}
+                />
+                <p>{testimonial[index].content}</p>
+                <span>{testimonial[index].writer}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
 
       <div className={styles.coursesContainerBg}>
-        <h3 className={styles.sectionTitle}>Top Featured Courses ⭐</h3>
+        <div className={styles.coursesHeading}>
+          <h3 className={styles.sectionTitle}>Top Featured Courses ⭐</h3>
+        </div>
+
         <div className={styles.coursesContainer}>
-          <div className={styles.courseCard}>
-            <div className={styles.courseImageContainer}>
-              <Image
-                src="/course1.webp"
-                alt="Course 1"
-                fill={true}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-            <h4>Beginner Korean *</h4>
-            <p>
-              Start your journey with the basics of Korean grammar and
-              vocabulary.
-            </p>
-          </div>
-          <div className={styles.courseCard}>
-            <div className={styles.courseImageContainer}>
-              <Image
-                src="/course2.webp"
-                alt="Course 2"
-                fill={true}
-                priority={true}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-            <h4>Intermediate Korean Conversations *</h4>
-            <p>
-              Dive into day-to-day dialogues and polish your speaking skills.
-            </p>
-          </div>
-          <div className={styles.courseCard}>
-            <div className={styles.courseImageContainer}>
-              <Image
-                src="/course3.webp"
-                alt="Course 3"
-                fill={true}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-            <h4>Advanced Korean Writing *</h4>
-            <p>
-              Master the art of writing in Korean with comprehensive lessons.
-            </p>
-          </div>
+          {course.map((content, index) => {
+            return (
+              <div className={styles.courseCard} key={index}>
+                <div className={styles.courseImageContainer}>
+                  <Image
+                    src={course[index].img}
+                    alt="Course 1"
+                    fill={true}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    style={{ objectFit: "cover" }}
+                    priority={true}
+                  />
+                </div>
+                <h4>{course[index].title}</h4>
+                <p>{course[index].content}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- util/data/data.tsx
+++ util/data/data.tsx
@@ -1,3 +1,77 @@
+import course1 from "../../public/course1.webp";
+import course2 from "../../public/course2.webp";
+import course3 from "../../public/course3.webp";
+
 const navbarList = ["HOME", "LEARNING", "COMMUNITY", "HELP & FAQ"];
 
-export { navbarList };
+const feature = [
+  {
+    id: 0,
+    title: "🟣 Adaptive Learning",
+    content:
+      "Experience tailored lessons that adapt to your individual learning pace.",
+  },
+  {
+    id: 1,
+    title: "🟠 Interactive Lessons",
+    content:
+      "Engage in lessons filled with dynamic quizzes, speaking exercises, and more.",
+  },
+  {
+    id: 2,
+    title: "🟢 Global Community",
+    content:
+      "Join a community of language learners and native speakers to practice with.",
+  },
+];
+
+const testimonial = [
+  {
+    id: 0,
+    content:
+      '"I\'ve never learned a language so efficiently! The adaptive learning really makes a difference."',
+    writer: "- Sarah, New York",
+  },
+  {
+    id: 1,
+    content:
+      '"The community here is so welcoming. I\'ve made friends and language partners from all over the world."',
+    writer: "- Jae, Seoul",
+  },
+];
+
+const course = [
+  {
+    id: 0,
+    img: course1,
+    title: "Beginner Korean *",
+    content:
+      "Start your journey with the basics of Korean grammar and vocabulary.",
+  },
+  {
+    id: 0,
+    img: course2,
+    title: "Intermediate Korean Conversations *",
+    content: "Dive into day-to-day dialogues and polish your speaking skills.",
+  },
+  {
+    id: 0,
+    img: course3,
+    title: "Advanced Korean Writing *",
+    content: "Master the art of writing in Korean with comprehensive lessons.",
+  },
+  {
+    id: 0,
+    img: course3,
+    title: "Advanced Korean Writing *",
+    content: "Master the art of writing in Korean with comprehensive lessons.",
+  },
+  {
+    id: 0,
+    img: course3,
+    title: "Advanced Korean Writing *",
+    content: "Master the art of writing in Korean with comprehensive lessons.",
+  },
+];
+
+export { navbarList, feature, testimonial, course };
