b779a17f2c03a6a3f6a032c8df10519665be1c2a
JunLee8108 <lejhn1@gmail.com>
2023-10-06 10:17:00 -0500

    quick fix

--- app/community/discussion/write/loading.tsx
+++ app/community/discussion/write/loading.tsx
@@ -3,11 +3,9 @@ import "./loading.css";
 export default function Loading() {
   return (
     <>
-      <div className="loading-bg">
-        <div className="loading-container-bg">
-          <div className="loading-container">
-            <div className="loader"></div>
-          </div>
+      <div className="loading-container-bg">
+        <div className="loading-container">
+          <div className="loader"></div>
         </div>
       </div>
     </>
